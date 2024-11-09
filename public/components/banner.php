e824ce6cb510aecb576e9d13c18163b997d500e8
itameio <itameios@gmail.com>
2024-11-09 14:42:06 +0200

    cleaned code2

--- public/components/banner.php
+++
@@ -1,7 +0,0 @@
-<div class="row banner">
-    <img class="logo" src="images/isterlogo.png" />
-    <div class="title">
-        <h1>مركز استرلاب</h1>
-        <p>للتدريب التقني والمنهجي</p>
-    </div>
-</div>
--- public/components/footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="footer">
-    <img class="footer-banner" src="images/isterbannerblack.png" />
-    <!-- <p>&copy; 2024 Isterlab Center. All rights reserved.</p> -->
-</footer>
